59272aa8226abf4e23994c7aaedbd03f6412472f
Караев Сергей <karaevsv@perfectsoft.ru>
2024-04-01 16:25:04 +0300

    123

---
+++ NTO_2024WPF/Classes/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTO_2024WPF.Classes
+{
+    public static class Helper
+    {
+       
+    }
+}
