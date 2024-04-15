4682e2e7eeaa02d2530e28955601e77eb35664b8
Караев Сергей <karaevsv@perfectsoft.ru>
2024-04-15 16:32:50 +0300

    excel ПОДГРУЖАЕТСЯ 👍👍👍👍👍👍:-D

--- NTO_2024WPF/Pages/Menu/EducationPage/TeacherPage.xaml.cs
+++ NTO_2024WPF/Pages/Menu/EducationPage/TeacherPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ExcelDataReader;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using NTO_2024WPF.data;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +27,8 @@ namespace NTO_2024WPF.Pages
     /// </summary>
     public partial class TeacherPage : Page
     {
+        private string file_name;
+
         public TeacherPage()
         {
             InitializeComponent();
@@ -45,7 +52,61 @@ namespace NTO_2024WPF.Pages
 
         private void ImportBtn_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog Dio = new OpenFileDialog();
+            Dio.Filter = "*.xls|*.xls";
+            Dio.Multiselect = false;
+            int count_first = 0;
+            if (Dio.ShowDialog() == true)
+            {
+                file_name = Dio.FileName;
+            }
 
+            using (var item = File.Open(file_name, FileMode.Open))
+            {
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                using (var reader = ExcelReaderFactory.CreateReader(item))
+                {
+                    var result = reader.AsDataSet();
+
+                    foreach (DataRow row in result.Tables[0].Rows)
+                    {
+                        try
+                        {
+                            if (count_first != 0)
+                            {
+                                Db.Studios.Load();
+                                var created_Studios = new Teacher();
+
+                                created_Studios.Fio = row[0].ToString();
+                                var searched_studio = Db.Teachers.FirstOrDefault(el => el.Fio == created_Studios.Fio);
+
+                                if (searched_studio != null)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    Db.Teachers.Add(created_Studios);
+                                    Db.SaveChanges();
+
+                                }
+
+                                LoadData();
+                            }
+                            else
+                            {
+                                count_first++;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Таблица не подходит по формату");
+                        }
+                    }
+                }
+            }
+            Db.Teachers.Load();
+            TeacherDG.ItemsSource = Db.Teachers.ToList();
         }
 
         private void LoadData()
--- NTO_2024WPF/Pages/Menu/EnterPage/SpacesPage.xaml.cs
+++ NTO_2024WPF/Pages/Menu/EnterPage/SpacesPage.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using ExcelDataReader;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using NTO_2024WPF.data;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static NTO_2024WPF.Classes.Helper;
 
 namespace NTO_2024WPF.Pages
 {
@@ -20,6 +27,8 @@ namespace NTO_2024WPF.Pages
     /// </summary>
     public partial class SpacesPage : Page
     {
+        private string file_name;
+
         public SpacesPage()
         {
             InitializeComponent();
@@ -41,6 +50,68 @@ namespace NTO_2024WPF.Pages
 
         }
 
-        
+        private void ImportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Dio = new OpenFileDialog();
+            Dio.Filter = "*.xls|*.xls";
+            Dio.Multiselect = false;
+            int count_first = 0;
+            if (Dio.ShowDialog() == true)
+            {
+                file_name = Dio.FileName;
+            }
+
+            using (var item = File.Open(file_name, FileMode.Open))
+            {
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                using (var reader = ExcelReaderFactory.CreateReader(item))
+                {
+                    var result = reader.AsDataSet();
+
+                    foreach (DataRow row in result.Tables[0].Rows)
+                    {
+                        try
+                        {
+                            if (count_first != 0)
+                            {
+                                Db.Spaces.Load();
+                                var created_Spaces = new Space();
+
+                                created_Spaces.Name = row[0].ToString();
+                                created_Spaces.Deskription = row[2].ToString();
+                                created_Spaces.Сapacity = int.Parse(row[1].ToString());
+                                 
+                                if (Db.Spaces.FirstOrDefault(el => el.Name == created_Spaces.Name && el.Deskription == created_Spaces.Deskription) != null)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    Db.Spaces.Add(created_Spaces);
+                                    Db.SaveChanges();
+                                }
+                                LoadData();
+                            }
+                            else
+                            {
+                                count_first++;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Таблица не подходит по формату");
+                        }
+
+                    }
+                }
+            }
+            Db.Spaces.Load();
+            SpaceDG.ItemsSource = Db.Spaces.ToList();
+        }
+
+        private void LoadData()
+        {
+            Db.Spaces.Load();
+        }
     }
 }
